21398f60898c8a87ab96a60142ca6f4db3b4f54b
Sneh Patel <patel.sneh2102@gmail.com>
2024-06-15 14:30:00 -0300

    Added Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import MaxWidthWrapper from './MaxWidthWrapper';
+import { useTranslations } from 'next-intl';
+
+const Footer = () => {
+  const t = useTranslations('footer');
+  const getYear = () => {
+    return new Date().getFullYear();
+  };
+  return (
+    <footer>
+      <MaxWidthWrapper>
+        <div className="relative z-10 mt-3 flex flex-col items-center justify-center border-t border-gray-200 py-6">
+          <div className="flex flex-row items-center text-center text-xs text-gray-500 dark:text-slate-200">
+            © {getYear()} by{' '}
+            <a
+              href="https://www.linkedin.com/in/patelsneh21"
+              target="_blank"
+              className="ml-1 text-gray-500 transition-all hover:text-gray-700 dark:text-slate-200 dark:hover:text-gray-200"
+            >
+              Sneh Patel
+            </a>
+            <div className="mx-2 h-5 border-r border-gray-400"></div>
+            <a
+              href="http://v1.snehpatel.tech/"
+              target="_blank"
+              className="text-gray-500 transition-all hover:text-gray-700 dark:text-slate-200 dark:hover:text-gray-200"
+            >
+              {t('versionOne')}
+            </a>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </footer>
+  );
+};
+
+export default Footer;
